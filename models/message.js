9b2a6025ff3718e2ebf9b6d61e10c7369deb4bfc
CameronTheDeveloper <Cameronposey@outlook.com>
2024-06-18 03:10:26 -0700

    Add date_posted formatted virtual property

--- models/message.js
+++ models/message.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
     title: { type: String, required: true, maxLength: 50 },
     text: { type: String, required: true, maxLength: 1000 },
     date_posted: { type: Date, default: Date.now }
+});
+
+MessageSchema.virtual('date_posted_formatted').get(function () {
+    return DateTime.fromJSDate(this.date_posted).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
