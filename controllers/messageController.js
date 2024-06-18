e4c7a3592e61058748f9d4e15a95cfee1d8f1728
CameronTheDeveloper <Cameronposey@outlook.com>
2024-06-18 14:29:16 -0700

    Add messageController.js skeleton handlers

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,1 +1,35 @@
 const Message = require('../models/message');
+const asyncHandler = require('express-async-handler');
+
+
+exports.message_list = asyncHandler(async (req, res, next) => {
+    res.send('message list display - not implemented');
+});
+
+exports.message_detail = asyncHandler(async (req, res, next) => {
+    res.send('message detail display - not implemented');
+});
+
+exports.message_create_get = asyncHandler(async (req, res, next) => {
+    res.send('message create GET - not implemented');
+});
+
+exports.message_create_post = asyncHandler(async (req, res, next) => {
+    res.send('message create POST - not implemented');
+});
+
+exports.message_delete_get = asyncHandler(async (req, res, next) => {
+    res.send('message delete GET - not implemented');
+});
+
+exports.message_delete_post = asyncHandler(async (req, res, next) => {
+    res.send('message delete POST - not implemented');
+});
+
+exports.message_edit_get = asyncHandler(async (req, res, next) => {
+    res.send('message edit GET - not implemented');
+});
+
+exports.message_edit_post = asyncHandler(async (req, res, next) => {
+    res.send('message edit POST - not implemented');
+});
