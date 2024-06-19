14f8ebe26399b63f8127845ff8fde8c60c0cf792
CameronTheDeveloper <Cameronposey@outlook.com>
2024-06-18 20:37:10 -0700

    Add message handler routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const message_controller = require('../controllers/messageController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', message_controller.message_list);
+
+router.get('/message/:id', message_controller.message_detail);
+
+router.get('/message/create', message_controller.message_create_get);
+router.post('/message/create', message_controller.message_create_post);
+
+router.get('/message/:id/delete', message_controller.message_delete_get);
+router.post('/message/:id/delete', message_controller.message_delete_post);
+
+router.get('/message/:id/edit', message_controller.message_edit_get);
+router.post('/message/:id/edit', message_controller.message_edit_post);
+
 
 module.exports = router;
