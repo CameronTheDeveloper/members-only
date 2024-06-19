7b896ab3da38b18b26f163c92c82ac03d1f17e85
CameronTheDeveloper <Cameronposey@outlook.com>
2024-06-19 15:42:33 -0700

    Add member form data sanitization

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,6 +1,6 @@
 const Member = require('../models/member');
 const asyncHandler = require('express-async-handler');
-
+const { body, validationResult } = require('express-validator');
 
 exports.member_list = asyncHandler(async (req, res, next) => {
     res.send('member list display - not implemented');
@@ -16,9 +16,38 @@ exports.member_create_get = asyncHandler(async (req, res, next) => {
     });
 });
 
-exports.member_create_post = asyncHandler(async (req, res, next) => {
-    res.send('member create POST - not implemented');
-});
+exports.member_create_post = [
+    body('memberFirstName')
+        .trim()
+        .isLength({ min: 1, max: 30 })
+        .escape()
+        .withMessage('First name must be 1 to 30 characters long.')
+        .isAlphanumeric()
+        .withMessage("First name has non-alphanumeric characters."),
+    body('memberLastName')
+        .trim()
+        .isLength({ min: 1, max: 35 })
+        .escape()
+        .withMessage('Last name must be 1 to 35 characters long.')
+        .isAlphanumeric()
+        .withMessage("Last name has non-alphanumeric characters."),
+    body('memberUsername')
+        .trim()
+        .isLength({ min: 1, max: 40 })
+        .escape()
+        .withMessage('Username must be 1 to 40 characters long.')
+        .isAlphanumeric()
+        .withMessage("Username has non-alphanumeric characters."),
+    body('memberPassword')
+        .trim()
+        .isLength({ min: 8, max: 128 })
+        .escape()
+        .withMessage('Password must be 8 to 128 characters long.'),
+
+    asyncHandler(async (req, res, next) => {
+        res.send('member create POST - not implemented');
+    })
+];
 
 exports.member_delete_get = asyncHandler(async (req, res, next) => {
     res.send('member delete GET - not implemented');
