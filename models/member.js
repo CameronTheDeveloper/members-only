44f25e56221b31111a53d781b4dceb042ff8f5f7
CameronTheDeveloper <Cameronposey@outlook.com>
2024-06-19 15:37:50 -0700

    Increase member model password max-length

--- models/member.js
+++ models/member.js
@@ -5,7 +5,7 @@ const MemberSchema = new Schema({
     first_name: { type: String, required: true, maxLength: 35 },
     last_name: { type: String, required: true, maxLength: 35 },
     username: { type: String, required: true, maxLength: 40 },
-    password: { type: String, required: true, maxLength: 75 },
+    password: { type: String, required: true, maxLength: 999 },
     member_status: { type: String, required: true, maxLength: 35 },
 });
 
